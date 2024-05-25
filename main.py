376872ab462dc302bd709b8e45c942caa2da0a7d
ved <ved.lucifer@gmail.com>
2024-05-25 16:10:00 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def summarize_text(text):
     result = summary(text)
     return result
 
-# Extract text from PDF file using PyPDF2
 def extract_text(filepath):
     with open(filepath,'rb') as f:
         reader = PdfReader(f)
@@ -36,14 +35,19 @@ def handle_document():
     st.subheader("Summarize Document")
     input_file = st.file_uploader("Upload your document", type=["pdf"])
     if st.button("Summarize Document") and input_file:
+        with open("doc_file.pdf", "wb") as f:
+            f.write(input_file.getbuffer())
         col1, col2 = st.columns([1,1])
         with col1:
-            st.markdown("*** Extracted text from document ***")
-            extracted_text = extract_text()
+            st.info("File uploaded successfully")
+            extracted_text = extract_text("doc_file.pdf")
+            st.markdown("**Extracted Text is Below:**")
+            st.info(extracted_text)
         with col2:
-            result = extract_text(input_file)
-            st.markdown("*** Summarize Document ***")
-            st.success(result)
+            st.markdown("**Summary Result**")
+            text = extract_text("doc_file.pdf")
+            doc_summary = summarize_text(text)
+            st.success(doc_summary)
 
 if choice == "Summarize Text":
     handle_text()
